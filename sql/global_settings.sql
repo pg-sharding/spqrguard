23090626ee8e6cfcde1afc426fc3016d39b5fe44
Yury Frolov <57130330+EinKrebs@users.noreply.github.com>
2025-12-16 18:22:55 +0500

    Add test for spqr_global_settings (#8)

---
+++ sql/global_settings.sql
@@ -0,0 +1,44 @@
+set allow_system_table_mods to true;
+CREATE EXTENSION spqrguard;
+
+CREATE TABLE not_spqr_t(i INT);
+CREATE TABLE spqr_d_t(i INT);
+CREATE TABLE spqr_ref_t(i INT);
+
+SELECT spqr_metadata.mark_distributed_relation ('spqr_d_t');
+SELECT spqr_metadata.mark_reference_relation ('spqr_ref_t');
+
+INSERT INTO not_spqr_t VALUES (1);
+INSERT INTO spqr_d_t VALUES (1);
+INSERT INTO spqr_ref_t VALUES (1);
+
+INSERT INTO spqr_metadata.spqr_global_settings (name, value) VALUES (42, 'yes');
+
+INSERT INTO not_spqr_t VALUES (2);
+
+INSERT INTO spqr_d_t VALUES (2);
+
+SELECT spqr_metadata.unmark_distributed_relation ('spqr_d_t');
+
+INSERT INTO spqr_d_t VALUES (3);
+
+DELETE FROM spqr_metadata.spqr_global_settings WHERE name = 42;
+INSERT INTO spqr_metadata.spqr_global_settings (name, value) VALUES (69, 'yes');
+
+INSERT INTO spqr_ref_t VALUES (2);
+
+UPDATE spqr_metadata.spqr_global_settings SET value = 'ok' WHERE name = 69;
+
+INSERT INTO spqr_ref_t VALUES (2);
+
+UPDATE spqr_metadata.spqr_global_settings SET value = 'on' WHERE name = 69;
+
+INSERT INTO spqr_ref_t VALUES (2);
+
+SELECT spqr_metadata.unmark_reference_relation ('spqr_ref_t');
+
+INSERT INTO spqr_ref_t VALUES (3);
+
+DROP TABLE not_spqr_t, spqr_d_t, spqr_ref_t;
+
+DROP EXTENSION spqrguard;
