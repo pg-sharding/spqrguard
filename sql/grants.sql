b8793e13a1aa597725afcf9245bd526a1028376f
denchick <denchick@yandex-team.ru>
2026-08-31 13:32:06 +0200

    fixes after review

--- sql/grants.sql
+++ sql/grants.sql
@@ -8,11 +8,7 @@ SELECT spqr_metadata.mark_distributed_relation ('spqr_d_t');
 SET ROLE spqrguard_reader;
 
 SELECT count(*) FROM spqr_metadata.spqr_distributed_relations;
-SELECT count(*) FROM spqr_metadata.spqr_reference_relations;
-SELECT count(*) FROM spqr_metadata.spqr_transferred_reference_relations;
 SELECT count(*) FROM spqr_metadata.spqr_local_key_ranges;
-SELECT count(*) FROM spqr_metadata.spqr_global_settings;
-SELECT count(*) FROM spqr_metadata.spqr_tx_status;
 
 -- read-only: metadata modification is still denied
 INSERT INTO spqr_metadata.spqr_distributed_relations VALUES (0);
